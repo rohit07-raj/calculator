578222d9c1873966640b038b62090d78f7c8901c
Rohit Raj <ritikraaz50@gmail.com>
2024-05-26 12:21:23 +0530

    code added

---
+++ README.md
@@ -0,0 +1,3 @@
+# About
+
+This is my project of Calculator.
---
+++ app.js
@@ -0,0 +1,28 @@
+let inp = document.querySelector("input");
+let btn = document.querySelectorAll("button");
+
+let string = "";
+
+let arr = Array.from(btn);
+
+arr.forEach(button => {
+    button.addEventListener("click", (e) =>{
+      if(e.target.innerHTML == "="){
+          string = eval(string);
+          inp.value = string;
+        } else if(e.target.innerHTML == "AC"){
+            string = "";
+            inp.value = string;
+        } else if(e.target.innerHTML == "DEL"){
+            string = string.substring(0,string.length-1);
+            inp.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            inp.value = string;
+        }
+    
+         
+        
+    })
+} )
